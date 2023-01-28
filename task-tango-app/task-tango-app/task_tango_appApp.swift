b19104c18f854378f43e20cee53c12eca9f985c1
Jeremy Liu <jliuu@umich.edu>
2023-01-28 13:47:15 -0500

    Tested React Native and begun project with SwiftUI

---
+++ task-tango-app/task-tango-app/task_tango_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_tango_appApp.swift
+//  task-tango-app
+//
+//  Created by Jeremy Liu on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct task_tango_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ task-tango-mobile/auth0-configuration.js
@@ -0,0 +1,6 @@
+const config = {
+  clientId: "kGfME5u35rAJlivtMNn27U9ob7woJ6uQ",
+  domain: "dev-vtvguykkngefawuw.us.auth0.com"
+};
+
+export default config;
