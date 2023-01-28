7db6fe9a87ae30732ef473ab33786140defaafba
Jeremy Liu <jliuu@umich.edu>
2023-01-28 17:37:56 -0500

    Refactored the code base to allow for multiple people to work on the base at once!

---
+++ tasktangomobile/src/Views/Home.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, ScrollView, StatusBar, Text, View } from "react-native";
+import { Colors, Header } from "react-native/Libraries/NewAppScreen";
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
