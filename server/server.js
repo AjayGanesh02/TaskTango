1adb5e5774385e3a06aa19b8ea03f1b4e24ac9bf
Ajay Ganesh <ajganesh2002@gmail.com>
2023-01-29 04:21:13 -0500

    changed port back

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 // get MongoDB driver connection
 const dbo = require('./db/conn');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 
