075c1c939333a6783a3207f27fa920ae46a15e4d
Jeremy Liu <jliuu@umich.edu>
2023-01-29 02:15:04 -0500

    Maybe there will be hope. Maybe not. Who knows. But all I know is that sleep will never happen tonight

--- next-task-tango-website/src/pages/[group]/index.tsx
+++ next-task-tango-website/src/pages/[group]/index.tsx
@@ -1,9 +1,38 @@
+import Head from "next/head";
+import {PlusIcon} from "@heroicons/react/20/solid";
+import GroupDisplay from "@/components/GroupDisplay";
+import {useState} from "react";
+
 
 const Group = () => {
+  const [groups, setGroups] = useState<string[]>(["thing1", "thing2", "thing3"])
+
   return (
-    <div>
-      <h1>this is Group</h1>
-    </div>
+    <>
+      <Head>
+        <title>X Group</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-full roboto select-none">
+        <h1 className="text-2xl font-bold pt-8 pl-10 flex">
+          Groups
+          <button
+            className="ml-auto mr-10"
+            onClick={() => console.log("Add group")}>
+            <PlusIcon className="w-8 h-8"/>
+          </button>
+        </h1>
+        <div className="pt-6 grid grid-cols-1 md:grid-cols-2">
+          {
+            groups.map((group, index) => {
+              return (
+                <GroupDisplay key={index} groupId={group}/>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
   )
 }
 
--- next-task-tango-website/src/pages/scan.tsx
+++ next-task-tango-website/src/pages/scan.tsx
@@ -1,9 +1,18 @@
+import Head from "next/head";
 
 const Scan = () => {
   return (
-    <div>
-      <h1>this is Scan</h1>
-    </div>
+    <>
+      <Head>
+        <title>Task Tango Scan</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen bg-dark-green roboto">
+        <h1 className="text-2xl text-center pt-40 font-bold text-brown">
+          Please scan your RFID tag
+        </h1>
+      </div>
+    </>
   )
 }
 
