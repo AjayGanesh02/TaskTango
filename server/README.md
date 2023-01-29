37272fc03dd288530c3e6eb9e5d0cd2b6f127fb1
Ajay Ganesh <ajganesh2002@gmail.com>
2023-01-29 03:08:51 -0500

    added to documentation and added users routes

--- server/README.md
+++ server/README.md
@@ -2,21 +2,32 @@
 
 ## Routes
 - GET
-  - tasks
+  - /tasks
     - query params:
-      - user: user_email
+      - user: user_email OR
       - group: group_id
-  - groups
+  - /groups
     - query params:
-      - user_email
+      - email: user's email
+  - /users
+    - query params:
+      - email: user's email
 - POSTS
-  - tasks
+  - /tasks
     - body
       - group: group id
       - initial_assignee: initial user's email
       - freq: int, minutes to remind
       - is_freq: bool, true for frequency based
-  - groups
+  - /groups
     - body
       - group_name: group name
       - initial_user: initial user's email
+  - /groups/add
+    - body
+      - group_id: group id
+      - new_user: new user's email
+  - /users
+    - body
+      - email: user's email
+      - phone: 10 digit phone num as a string w no spaces
---
+++ server/routes/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+// crudRoutes is an instance of the express router.
+// We use it to define our routes.
+const userRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require('../db/conn');
+
+userRoutes.route('/users').get(async function (req, res) {
+  const dbConnect = dbo.getDb();
+  const user = req.query.email
+  if (!user) {
+    res.send("Specify a user email to get phone num")
+  }
+  dbConnect
+    .collection('Users')
+    .find({
+      'email': user
+    })
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+userRoutes.route('/users').post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const matchDocument = {
+    email: req.body.email,
+    phone: "1" + req.body.phone
+  };
+
+  console.log(matchDocument)
+
+  dbConnect
+    .collection('Users')
+    .insertOne(matchDocument, function (err, result) {
+      if (err) {
+        res.status(400).send('Error inserting matches!');
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
+
+module.exports = userRoutes;
