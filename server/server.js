701b69df3b43ceeb8d4630b619f5e57eaeaf6780
Ajay Ganesh <ajganesh2002@gmail.com>
2023-01-29 05:27:28 -0500

    removed cors

--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ require('dotenv').config({ path: './config.env' });
 
 // Requires for express and auth0
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
 // get MongoDB driver connection
 const dbo = require('./db/conn');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(cors());
+
+// app.use(cors());
 app.use(express.json());
 app.use(require('./routes/groups'));
 app.use(require('./routes/tasks'));
